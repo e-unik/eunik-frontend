ff15a1d9e97128fcb3bf8070b88884e68aa79180
Rahcheev Ilya <i.s.rakhcheev@gmail.com>
2025-07-31 16:40:07 +0300

    add tg button to login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
 import Login from './view/Login/Login'
 import Register from './view/Register/Register'
-import TelegramAuth from './view/Login/LoginTelegramWidget'
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/tgauth" element={<TelegramAuth />} />
       </Routes>
     </BrowserRouter>
   )
